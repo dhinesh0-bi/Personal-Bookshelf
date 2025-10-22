79e6b7ff6b8a0fddcc37054ab6c43eb0e1f2623e
dhinesh0-bi <kdhinesh2005@gmail.com>
2025-10-22 10:00:29 +0530

    delete

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,10 +3,12 @@ import Header from '../components/Header';
 import BookUploadForm from '../components/BookUploadForm';
 import BookList from '../components/BookList';
 
+// src/pages/Dashboard.jsx
+
 export default function Dashboard() {
   const [refreshKey, setRefreshKey] = useState(0);
 
-  const handleBookUploaded = () => {
+  const handleBookAction = () => {
     setRefreshKey(oldKey => oldKey + 1);
   };
 
@@ -14,8 +16,14 @@ export default function Dashboard() {
     <>
       <Header title="Dashboard" />
       <div className="content-area">
-        <BookUploadForm onBookUploaded={handleBookUploaded} />
-        <BookList shouldRefresh={refreshKey} />
+        {/* "onBookUploaded" is passed to the form */}
+        <BookUploadForm onBookUploaded={handleBookAction} />
+        
+        {/* "onBookAction" must be passed to the list */}
+        <BookList 
+          shouldRefresh={refreshKey} 
+          onBookAction={handleBookAction} // <-- MAKE SURE THIS LINE EXISTS
+        />
       </div>
     </>
   );
